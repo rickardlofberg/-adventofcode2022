cb451ed6dd835817c229129b087ac7c5dcc07999
rickardlofberg <rickard.lofberg@chassigroup.com>
2024-12-01 11:20:28 +0100

    Fix package names

--- pkg/util/solver.go
+++ pkg/util/solver.go
@@ -8,7 +8,7 @@ type fn func([]string) (int64, error)
 
 func GetSolver(problem string) fn {
 	var solvers = map[string]fn{
-		"1.1":  day1.Part1
+		"1.1": day1.Part1,
 	}
 	return solvers[problem]
 }
